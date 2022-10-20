1ea2b4f96f5944e07e4704be3dd7a3868a61553a
Chidimma83 <kanyhills@yahoo.com>
2022-10-19 18:45:27 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+   ** main - Entry point
+  **
+  ** Description: prints_putchar
+  **
+  ** Return: Always 0(succes)
+  **/
+
+int main(void)
+{
+	putchar('D');
+	return 0;
+}
